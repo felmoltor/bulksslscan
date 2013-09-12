3bb8e7c502716d4f42571ddc8ea7ee7c356242b2
Felipe Molina <felmoltor@gmail.com>
2013-09-12 18:41:16 +0200

    Add output file in CSV functionality

--- bulksslscan.sh
+++ bulksslscan.sh
@@ -73,12 +73,21 @@ function searchCBCMethod(){
 ## MAIN ##
 ##########
 
+# ARG 1
 if [[ -f $1 ]]
 then
 	IP_FILE=$1
 else
-	echo "Usage: $0 <ip:port_list_file>"
+	echo "Usage: $0 <ip:port_list_file> [<output_file>]"
 	exit 1
+fi
+
+# ARG 2
+if [[ "$2" == "" ]]
+then
+    OUTPUT_FILE="results.csv"
+else
+    OUTPUT_FILE=$2
 fi
 
 hasSSLv2=0
@@ -89,11 +98,13 @@ cont=1
 beast_cbc=0
 
 
+echo "IP;Key Len (>= 128 bits);SSLv2 Disabled;CBC Disabled (SSLv3,TLSv1)" > $OUTPUT_FILE
+
 for ip in `cat $IP_FILE`
 do
     echo 
 	echo "Scanning $ip ($cont/$total_ips). Please wait..."
-	sslscan --no-failed --xml=results/$ip.out.xml $ip > /dev/null
+	# sslscan --no-failed --xml=results/$ip.out.xml $ip > /dev/null
 	# Seach for cipher protocols accepted
 	ciphers=$(grep '<cipher status="accepted" sslversion="' results/$ip.out.xml | cut -f5 -d' ' | cut -f2 -d= | tr -d '"' | sort -u )
     smalestkeylen=$(grep "<cipher status=\"accepted\" sslversion=\"" results/$ip.out.xml | cut -f6 -d' ' | cut -f2 -d'=' | tr -d '"' | sort -u --numeric-sort | head -n1)
@@ -118,8 +129,10 @@ do
     if [[ $hasSSLv2 == 1 ]]
     then
         red "SSLv2 detected"
+        sslv2_status="FAIL"
     else
         green "SSLv2 was not detected"
+        sslv2_status="OK"
     fi
 
     # Is the keylength 128 bits or more?
@@ -128,18 +141,24 @@ do
     if [[ $hasMinimum == 0 ]]
     then
         red "The minimum length of cipher keys not correct ($smalestkeylen bits)"
+        min_len_status="FAIL"
     else
         green "The minimum length of cipher keys is correct ($smalestkeylen bits)"
+        min_len_status="OK"
     fi
 
     if [[ $beast_cbc == 1 ]]
     then
         red "This host is not protected against BEAST (Uses CBC with TLSv1 or SSLv3)"
+        beast_status="FAIL"
     else
         green "This host is protected against BEAST (Does not use CBC with TLSv1 or SSLv3)"
+        beast_status="OK"
     fi
 	
     cont=$(( cont+1 ))
+
+    echo "$ip;$min_len_status;$sslv2_status;$beast_status" >> $OUTPUT_FILE
 done
 
 
